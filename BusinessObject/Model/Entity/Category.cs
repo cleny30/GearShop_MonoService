95ca330810b8c56493f35166e509a6c85764d7de
cleny30 <kushirotm@gmail.com>
2024-06-06 07:27:58 +0700

    feat: ini businessObject

---
+++ BusinessObject/Model/Entity/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObject.Model.Entity;
+
+public partial class Category
+{
+    public int CateId { get; set; }
+
+    public string CateName { get; set; } = null!;
+
+    public bool IsAvailable { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
