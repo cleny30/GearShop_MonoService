edb8c0853444cccbd3d363a0700b4c1122e8ca1e
cleny30 <kushirotm@gmail.com>
2024-06-06 13:16:06 +0700

    modify: add keyword category

--- BusinessObject/Model/Entity/Category.cs
+++ BusinessObject/Model/Entity/Category.cs
@@ -9,6 +9,8 @@ public partial class Category
 
     public string CateName { get; set; } = null!;
 
+    public string Keyword { get; set; } = null!;
+
     public bool IsAvailable { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
