b7e81f997be99510d496404050c0292404365999
cleny30 <kushirotm@gmail.com>
2024-06-07 15:22:25 +0700

    feat: add DataResult, Error, ValidateResult
    refactor: Remove model in web applciation

--- GearShopWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GearShopWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
