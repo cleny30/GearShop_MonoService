424459e98782380a02eed8256bf129174c84e732
Nguyen Huu Duy <139975518+UnkeptHaroldd@users.noreply.github.com>
2024-06-18 08:57:20 +0700

    add

--- DataAccess/Repository/OrderRepository.cs
+++ DataAccess/Repository/OrderRepository.cs
@@ -1,4 +1,7 @@
-﻿using DataAccess.IRepository;
+﻿using BusinessObject.Model.Entity;
+using BusinessObject.Model.Page;
+using DataAccess.IRepository;
+using ISUZU_NEXT.Server.Core.Extentions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,53 @@ namespace DataAccess.Repository
 {
     public class OrderRepository : IOrderRepository
     {
+
+        public List<OrderModel> GetOrderList()
+        {
+            List<Order> orders;
+            try
+            {
+                var dbContext = new PrndatabaseContext();
+                orders = dbContext.Orders.ToList();
+                List<OrderModel> _orders = new List<OrderModel>();
+                foreach (var order in orders)
+                {
+                    OrderModel _order = new OrderModel();
+                    _order.CopyProperties(order);
+                    _orders.Add(_order);
+                }
+                return _orders;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public OrderModel GetOrderByID(string ID)
+        {
+            Order order;
+            try
+            {
+                var dbContext = new PrndatabaseContext();
+                order = dbContext.Orders.Where(o => o.OrderId == ID).SingleOrDefault();
+                if(order != null)
+                {
+                    OrderModel _order = new OrderModel
+                    {
+
+                    };
+
+                    return _order;
+                } else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
     }
 }
